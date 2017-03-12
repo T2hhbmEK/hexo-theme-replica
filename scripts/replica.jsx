7b51808850f9392be502f9a19aa600b314aeee7b
crucifyml <i@crucifyml.com>
2017-03-12 08:30:32 +0800

    Archives, tags & categories.

--- scripts/replica.jsx
+++ scripts/replica.jsx
@@ -4,11 +4,25 @@ var pagination = require('hexo-pagination');
 // post = Document { title, date: moment, slug, layout, ...} depends on layout type
 
 hexo.extend.helper.register('is_about', function(){
-  return Boolean(this.page.__index);
+  return Boolean(this.page.__about);
 });
-
-hexo.theme.setView(hexo.route.format('about/'),{
-  data: 'about',
-  __about: true,
-  modified: false,
+hexo.extend.generator.register('about', function(locals){
+  let has_about = false;
+  locals.pages.forEach((page, index) => {
+    if (page.path == 'about/index.html') {
+      has_about = true;
+      page.__about = true;
+    }
+  });
+  if (has_about) return;
+  return {
+    path: 'about/index.html',
+    data: {
+      content: 'Nothing here. Please edit `about/index.md`',
+      __about: true,
+    },
+    layout: 'about'
+  };
+});
+hexo.extend.filter.register('template_locals', function(locals){
 });
